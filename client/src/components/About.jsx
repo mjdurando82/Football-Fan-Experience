3f360f5f6a8c27ea2cecab24cf604dcc661bb859
Michael Durando <mjdurando82@yahoo.com>
2023-02-14 14:00:10 -0500

    about page fix

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
     <div className="about">
       <h1>About Page</h1>
       <big>
-        Check out all 30 Football Stadiums! Leave a brief review if you have been to a game at any of the stadiums listed. You can also see other reviews by clicking on the stadium that interests you! 
+        Check out all 30 Football Stadiums! Leave a brief review if you've attended a game at any of the stadiums listed. You can also see other reviews by clicking on the stadium that interests you! 
       </big>
     </div>
   )
