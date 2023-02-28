a57af238ff2844bb933d5e7c6004202fa8523cff
Michael Durando <mjdurando82@yahoo.com>
2023-02-28 09:36:56 -0500

    heroku

--- db/index.js
+++ db/index.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose')
-require('dotenv').config()
+require('dotenv').config() // Add this line
 
 mongoose
   .connect(process.env.MONGODB_URI)
   .then(() => {
-    console.log('Successfully connected to MongoDB.')
+    console.log('Successfully connected to MongoDB!')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
-
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
